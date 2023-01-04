403d59e720eba8a7e34672435bd231b1affb90e4
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-01-04 12:23:40 +0330

    Some Bugs Fixed.

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,6 +1,14 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class FileManager {
     public String Fileroot;
 
@@ -61,6 +69,44 @@ public class FileManager {
                 file.renameTo(new File(toDir, file.getName()));
             }
         }
+    }
+
+    public void printFiles(File[] files) {
+        for (File file : files) {
+            long m = getFileCreationEpoch(file);
+            Instant instant = Instant.ofEpochMilli(m);
+            LocalDateTime date = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+            System.out.println(date+" - "+file.getName());
+        }
+    }
+
+    public void sortFilesByDateCreated (File[] files) {
+        Arrays.sort(files, new Comparator<File>() {
+            public int compare (File f1, File f2) {
+                long l1 = getFileCreationEpoch(f1);
+                long l2 = getFileCreationEpoch(f2);
+                return Long.valueOf(l1).compareTo(l2);
+            }
+        });
+    }
+
+    public long getFileCreationEpoch (File file) {
+        try {
+            BasicFileAttributes attr = Files.readAttributes(file.toPath(),
+                    BasicFileAttributes.class);
+            return attr.creationTime()
+                    .toInstant().toEpochMilli();
+        } catch (IOException e) {
+            throw new RuntimeException(file.getAbsolutePath(), e);
+        }
+    }
+    public void sorter(File dir) {
+        File[] files = dir.listFiles();
+        System.out.println("-- printing files before sort --");
+        printFiles(files);
+        sortFilesByDateCreated(files);
+        System.out.println("-- printing files after sort --");
+        printFiles(files);
     }
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         FileManager f = new FileManager();
-        f.folder_create("G:\\","Folder");
-        f.move(new File("G:\\Folder"), new File("G:\\Folder1"));
+        f.sorter(new File("G:\\Folder"));
 
 
 

a3ac2e897e5becd7676ee91e438594937b5942d3
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-01-04 11:54:28 +0330

    Started!

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 public class FileManager {
@@ -8,7 +9,7 @@ public class FileManager {
         String FileFullName = Filename + "." + FileType;
         try {
             FileWriter myWriter = new FileWriter(FileFullName);
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
@@ -16,6 +17,50 @@ public class FileManager {
             e.printStackTrace();
         }
 
+    }
+    public void file_delete(String Filename , String FileType){
+        String FileFullName = Filename + "." + FileType;
+        File myObj = new File(FileFullName);
+        if (myObj.delete()) {
+            System.out.println("Deleted the file: " + myObj.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+    }
+
+    public void folder_create(String Path,String FolderName){
+        String path = Path;
+        //Using Scanner class to get the folder name from the user
+        path = path+FolderName;
+        //Instantiate the File class
+        File f1 = new File(path);
+        //Creating a folder using mkdir() method
+        boolean bool = f1.mkdir();
+        if(bool){
+            System.out.println("Folder is created successfully");
+        }else{
+            System.out.println("Error Found!");
+        }
+
+    }
+
+    public void folder_delete(String Path){
+        File myObj = new File(Path);
+        if (myObj.delete()) {
+            System.out.println("Deleted the folder: " + myObj.getName());
+        } else {
+            System.out.println("Failed to delete the folder.");
+        }
+    }
+
+    public void move(File toDir, File currDir) {
+        for (File file : currDir.listFiles()) {
+            if (file.isDirectory()) {
+                move(toDir, file);
+            } else {
+                file.renameTo(new File(toDir, file.getName()));
+            }
+        }
     }
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         FileManager f = new FileManager();
-        f.file_create("1","py");
+        f.folder_create("G:\\","Folder");
+        f.move(new File("G:\\Folder"), new File("G:\\Folder1"));
 
 
 
